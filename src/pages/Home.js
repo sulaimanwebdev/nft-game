59b07d26e64d137717693fbd748e81f63439126c
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-10-14 13:37:47 +0500

    new

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,29 +21,37 @@ const Home = () => {
 <div className="parent information">
     <img src="images/topTexture.png" alt="" className="topTexture"/>
     <img src="images/bottomTexture.png" alt="" className="bottomTexture"/>
+    <div className="title"><h3>Episode 0 : The Prophecy</h3><img src="images/underline.png" alt="" /></div>
 
 <div className="child informationC">
 <img src="/images/bbb.png" alt="" className="transparent"/>
 
-    <div className="inText">
-        <p> <span>Episode 0 : The Prophecy <img src="/images/topsmall1.png" alt="" /></span>
 
-There will come a day
+    <div className="inText">
+        <p>
+
+        There will come a day
 When darkness finds a clearway
 When the stars start to shiver
 When the earth will quiver
+<br /><br />
 That day no one will be safe
 For the wars will erupt everywhere
 That day, the prince, just like the waif
 Will see the abyss of despair
+<br /><br />
+
 9999 warriors, chosen long before
 Will stand as one at hell's door
 For different reasons and from different routes
 Some for glory, some for family, and some just for loots
+<br /><br />
+
 A war of unknown consequences, it is your destiny
 Gather your strength, your wit and your pride
 Your first labor is to outsmart your enemy,
 Decypher their locks, open their gates wide
+<br /><br />
 Enter their realm .. <br/>
 <img src="/images/topsmall1.png" alt="" className="bottomLine"/>
 </p>
@@ -61,6 +69,11 @@ Enter their realm .. <br/>
     <div className="child collectionC">
          <div className="title"><h3>THE COLLECTION</h3><img src="images/underline.png" alt="" /></div>
          <div className="collectionCont">
+             <div className="collectionMint">
+                 <h3>Sneak Peek</h3>
+                 <p>The Races</p>
+                 <a href="https://discord.com/invite/et874VqvEy">WANNA MINT?</a>
+             </div>
              <img src="/images/gif_the_collection.gif" alt="" />
          </div>
     </div>
@@ -174,7 +187,7 @@ Nobody likes gas wars, this is why our minting process revolves around a game of
 
 <div className="social">
 <a href="#"><img src="/images/logo_9tales.png" alt="" /></a>
-<a href="https://discord.com/UWkz3p7bwe" target="_blank"><svg height="1828" viewBox="-10.63 -.07077792 823.87 610.06955549" width="2500" xmlns="http://www.w3.org/2000/svg"><path d="m678.27 51.62c90.35 132.84 134.97 282.68 118.29 455.18-.07.73-.45 1.4-1.05 1.84-68.42 50.24-134.71 80.73-200.07 100.95a2.55 2.55 0 0 1 -2.81-.95c-15.1-21.01-28.82-43.16-40.84-66.42-.69-1.37-.06-3.02 1.36-3.56 21.79-8.21 42.51-18.05 62.44-29.7 1.57-.92 1.67-3.17.22-4.25-4.23-3.14-8.42-6.44-12.43-9.74-.75-.61-1.76-.73-2.61-.32-129.39 59.75-271.13 59.75-402.05 0-.85-.38-1.86-.25-2.59.35-4 3.3-8.2 6.57-12.39 9.71-1.45 1.08-1.33 3.33.25 4.25 19.93 11.43 40.65 21.49 62.41 29.74 1.41.54 2.08 2.15 1.38 3.52-11.76 23.29-25.48 45.44-40.86 66.45-.67.85-1.77 1.24-2.81.92-65.05-20.22-131.34-50.71-199.76-100.95-.57-.44-.98-1.14-1.04-1.87-13.94-149.21 14.47-300.29 118.18-455.18.25-.41.63-.73 1.07-.92 51.03-23.42 105.7-40.65 162.84-50.49 1.04-.16 2.08.32 2.62 1.24 7.06 12.5 15.13 28.53 20.59 41.63 60.23-9.2 121.4-9.2 182.89 0 5.46-12.82 13.25-29.13 20.28-41.63a2.47 2.47 0 0 1 2.62-1.24c57.17 9.87 111.84 27.1 162.83 50.49.45.19.82.51 1.04.95zm-339.04 283.7c.63-44.11-31.53-80.61-71.9-80.61-40.04 0-71.89 36.18-71.89 80.61 0 44.42 32.48 80.6 71.89 80.6 40.05 0 71.9-36.18 71.9-80.6zm265.82 0c.63-44.11-31.53-80.61-71.89-80.61-40.05 0-71.9 36.18-71.9 80.61 0 44.42 32.48 80.6 71.9 80.6 40.36 0 71.89-36.18 71.89-80.6z" fill="white"/></svg></a>
+<a href="https://discord.com/invite/et874VqvEy" target="_blank"><svg height="1828" viewBox="-10.63 -.07077792 823.87 610.06955549" width="2500" xmlns="http://www.w3.org/2000/svg"><path d="m678.27 51.62c90.35 132.84 134.97 282.68 118.29 455.18-.07.73-.45 1.4-1.05 1.84-68.42 50.24-134.71 80.73-200.07 100.95a2.55 2.55 0 0 1 -2.81-.95c-15.1-21.01-28.82-43.16-40.84-66.42-.69-1.37-.06-3.02 1.36-3.56 21.79-8.21 42.51-18.05 62.44-29.7 1.57-.92 1.67-3.17.22-4.25-4.23-3.14-8.42-6.44-12.43-9.74-.75-.61-1.76-.73-2.61-.32-129.39 59.75-271.13 59.75-402.05 0-.85-.38-1.86-.25-2.59.35-4 3.3-8.2 6.57-12.39 9.71-1.45 1.08-1.33 3.33.25 4.25 19.93 11.43 40.65 21.49 62.41 29.74 1.41.54 2.08 2.15 1.38 3.52-11.76 23.29-25.48 45.44-40.86 66.45-.67.85-1.77 1.24-2.81.92-65.05-20.22-131.34-50.71-199.76-100.95-.57-.44-.98-1.14-1.04-1.87-13.94-149.21 14.47-300.29 118.18-455.18.25-.41.63-.73 1.07-.92 51.03-23.42 105.7-40.65 162.84-50.49 1.04-.16 2.08.32 2.62 1.24 7.06 12.5 15.13 28.53 20.59 41.63 60.23-9.2 121.4-9.2 182.89 0 5.46-12.82 13.25-29.13 20.28-41.63a2.47 2.47 0 0 1 2.62-1.24c57.17 9.87 111.84 27.1 162.83 50.49.45.19.82.51 1.04.95zm-339.04 283.7c.63-44.11-31.53-80.61-71.9-80.61-40.04 0-71.89 36.18-71.89 80.61 0 44.42 32.48 80.6 71.89 80.6 40.05 0 71.9-36.18 71.9-80.6zm265.82 0c.63-44.11-31.53-80.61-71.89-80.61-40.05 0-71.9 36.18-71.9 80.61 0 44.42 32.48 80.6 71.9 80.6 40.36 0 71.89-36.18 71.89-80.6z" fill="white"/></svg></a>
                     <a href="https://twitter.com/9tales_NFT" target="_blank"><i class="fab fa-twitter"></i></a>
                     <a href="#"><i class="fab fa-instagram"></i></a>
 </div>
